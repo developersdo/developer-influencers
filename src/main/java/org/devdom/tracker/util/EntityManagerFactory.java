415ff293eda2da6b2c62b99e0ef3b5f1ede9d775
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-24 08:12:09 -0400

    Agregando Factory

---
+++ src/main/java/org/devdom/tracker/util/EntityManagerFactory.java
@@ -0,0 +1,22 @@
+package org.devdom.tracker.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Carlos Vasquez Polanco
+ */
+public class EntityManagerFactory{
+    
+    private final String PERSISTENCE_UNIT = "tracker_pu";
+    private final javax.persistence.EntityManagerFactory emf;
+
+    public EntityManagerFactory(){
+        emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, Configuration.JPAConfig());
+    }
+
+    public EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+}
