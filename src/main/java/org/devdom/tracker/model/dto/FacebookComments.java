2f59648e2792fe8ad43b296ae2de2a48cd5daf00
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-24 02:15:35 -0400

    creando entidad para manejar mensajes

---
+++ src/main/java/org/devdom/tracker/model/dto/FacebookComments.java
@@ -0,0 +1,125 @@
+package org.devdom.tracker.model.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Carlos Vásquez Polanco
+ */
+@Entity
+@XmlRootElement
+public class FacebookComments implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Column(name = "creation_date")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date createTime;
+    
+    @Column(name = "like_count")
+    private Integer likeCount;
+    
+    @Column(name = "from_id")
+    private String fromId;
+    
+    @Column(name = "mesage")
+    private String message;
+    
+    @Id
+    @Column(name = "message_id")
+    private String messageId;
+    
+    @Column(name = "post_id")
+    private String postId;
+
+    /**
+     * @return the createTime
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * @param createTime the createTime to set
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * @return the likeCount
+     */
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    /**
+     * @param likeCount the likeCount to set
+     */
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    /**
+     * @return the fromId
+     */
+    public String getFromId() {
+        return fromId;
+    }
+
+    /**
+     * @param fromId the fromId to set
+     */
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the messageId
+     */
+    public String getMessageId() {
+        return messageId;
+    }
+
+    /**
+     * @param messageId the messageId to set
+     */
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    /**
+     * @return the postId
+     */
+    public String getPostId() {
+        return postId;
+    }
+
+    /**
+     * @param postId the postId to set
+     */
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+}
