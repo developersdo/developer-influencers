504962324ad0dd7089b3efa6461ba0f7cebccd81
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-24 02:06:52 -0400

    Creando entidad para manejar los posts

---
+++ src/main/java/org/devdom/tracker/model/dto/FacebookPost.java
@@ -0,0 +1,102 @@
+package org.devdom.tracker.model.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Carlos Vásquez Polanco
+ */
+@Entity
+@XmlRootElement
+public class FacebookPost implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "post_id")
+    private String postId;
+    
+    @Column(name = "from_id")
+    private String fromId;
+    
+    @Column(name = "message")
+    private String message;
+    
+    @Column(name = "like_count")
+    private Integer likeCount;
+    
+    @Column(name = "creation_date")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date creationDate;
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    /**
+     * @return the fromId
+     */
+    public String getFromId() {
+        return fromId;
+    }
+
+    /**
+     * @param fromId the fromId to set
+     */
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the likeCount
+     */
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    /**
+     * @param likeCount the likeCount to set
+     */
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    /**
+     * @return the creationDate
+     */
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    /**
+     * @param creationDate the creationDate to set
+     */
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+    
+}
