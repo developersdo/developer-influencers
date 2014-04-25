74be477e152f6c0426fb9a99b012cd198836bdc4
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-25 08:17:22 -0400

    sincronizando informacion de post con facebook

--- src/main/java/org/devdom/tracker/model/dao/FacebookDao.java
+++ src/main/java/org/devdom/tracker/model/dao/FacebookDao.java
@@ -1,28 +1,71 @@
 package org.devdom.tracker.model.dao;
 
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.Post;
+import facebook4j.Reading;
+import facebook4j.ResponseList;
+import facebook4j.conf.ConfigurationBuilder;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
-import org.devdom.tracker.util.EntityManagerFactory;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.devdom.client.facebook.FBConnect;
+import org.devdom.tracker.model.dto.FacebookPost;
+import org.devdom.tracker.util.Configuration;
 
 /**
  *
  * @author Carlos Vásquez Polanco
  */
 public class FacebookDao {
-    
-    private final EntityManagerFactory emf;
-    
-    public FacebookDao(){
-        emf = new EntityManagerFactory();
-    }
+
+    private static final long serialVersionUID = 1L;
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    private static final ConfigurationBuilder cb = Configuration.getFacebookConfig();
+    public FacebookDao(){ }
 
     public EntityManager getEntityManager(){
-        return emf.getEntityManager();
+        return emf.createEntityManager(Configuration.JPAConfig());
     }
     
     public void syncInformation(){
         EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        
-        em.getTransaction().commit();
+        Facebook fb = new FacebookFactory(cb.build()).getInstance();
+
+        try {
+            em.getTransaction().begin();
+            Reading reading = new Reading();
+            reading.limit(250);
+            ResponseList<Post> group = fb.getGroupFeed("201514949865358",reading);
+
+            List<Post> posts = group.subList(1,group.size());            
+            List<FacebookPost> list = em.createNamedQuery("FacebookPost.findAll").getResultList();
+
+            posts.stream().forEach((post) -> {                
+                FacebookPost newPost = new FacebookPost();
+                newPost.setPostId(post.getId().split("_")[1]);
+                newPost.setFromId(post.getFrom().getId());
+                newPost.setCreationDate(post.getCreatedTime());
+                newPost.setLikeCount(post.getLikes().size());
+                newPost.setMessage(post.getMessage());
+
+                boolean exists = false;
+                for(FacebookPost postSaved : list){
+                    if(postSaved.getFromId().equals(newPost.getFromId())){
+                        exists = true;
+                        break;
+                    }
+                }
+                if(!exists)
+                    em.persist(newPost);
+            });
+            em.getTransaction().commit();
+        } catch (FacebookException ex) {
+            Logger.getLogger(FBConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
