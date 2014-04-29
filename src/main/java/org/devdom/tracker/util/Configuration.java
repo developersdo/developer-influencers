12a02548f7b5abf69a155522a7f97fc00af11c96
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-29 02:05:45 -0400

    correción en declaración de variable

--- src/main/java/org/devdom/tracker/util/Configuration.java
+++ src/main/java/org/devdom/tracker/util/Configuration.java
@@ -9,12 +9,12 @@ import java.util.Map;
  * @author Carlos Vásquez Polanco
  */
 
-public class Configuration{
+public class Configuration{ 
 
     private Configuration(){ }
 
     public static int POSTS_LIMIT = 4000; //Determina la cantidad máxima de post que se retornarán
-    pulic static int LIKES_LIMIT = 300; // Determina la cantidad máxima de likes que se quiere buscar
+    public static int LIKES_LIMIT = 300; // Determina la cantidad máxima de likes que se quiere buscar
     public static String JPA_PU = "jpa";
     public static String[] SEEK_GROUPS = {"201514949865358"};
     
