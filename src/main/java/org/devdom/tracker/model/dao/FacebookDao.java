cf408ffe26bab89d077d499f0d5af45c81400b39
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-30 07:50:40 -0400

    Utilizando una instancia global de facebook

--- src/main/java/org/devdom/tracker/model/dao/FacebookDao.java
+++ src/main/java/org/devdom/tracker/model/dao/FacebookDao.java
@@ -16,6 +16,7 @@ import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.http.HttpServletRequest;
 import org.devdom.tracker.model.dto.FacebookComment;
 import org.devdom.tracker.model.dto.FacebookPost;
 import org.devdom.tracker.util.Configuration;
@@ -29,7 +30,8 @@ public class FacebookDao {
     private static final long serialVersionUID = 1L;
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
     private static final ConfigurationBuilder cb = Configuration.getFacebookConfig();
-    private final Facebook fb = new FacebookFactory(cb.build()).getInstance();
+    //Facebook facebook = (Facebook) request.getSession().getAttribute("facebook");
+    Facebook facebook = new FacebookFactory(cb.build()).getInstance();
     public FacebookDao(){ }
 
     public EntityManager getEntityManager(){
@@ -41,7 +43,7 @@ public class FacebookDao {
         PagableList<Comment> comments = post.getComments();
         comments.parallelStream().forEach((comment) -> {
             if( (comment.getId()!=null) && (post.getId()!=null) && (comment.getFrom()!=null) ){
-                int likes = getCommentLikes(comment);
+                int likes = this.getCommentLikes(comment);
                 FacebookComment newComment = new FacebookComment();            
                 newComment.setCreateTime(comment.getCreatedTime());
                 newComment.setLikeCount(likes);
@@ -57,20 +59,20 @@ public class FacebookDao {
     /**
     * Sincroniza la información de facebook para cada developer
     *
+     * @param request request from servlet
     * @param groupId identificador del grupo que se desea revisar
     */
-    public void syncGroupInformation(String groupId){
+    public void syncGroupInformation(HttpServletRequest request, String groupId){
         EntityManager em = getEntityManager();
 
         try {
             em.getTransaction().begin();
             Reading reading = new Reading();
             reading.limit(Configuration.POSTS_LIMIT);
-            ResponseList<Post> group = fb.getGroupFeed(groupId,reading);
+            ResponseList<Post> group = facebook.getGroupFeed(groupId,reading);
 
             List<Post> posts = group.subList(1,group.size());
-            posts.stream().forEach((post) -> {
-                
+            posts.stream().forEach((Post post) -> {
                 if(post.getFrom()!=null){
                     int likes = getPostLikes(post);
                     FacebookPost newPost = new FacebookPost();
@@ -88,21 +90,24 @@ public class FacebookDao {
             em.getTransaction().commit();
         } catch (FacebookException | IllegalArgumentException ex) {
             Logger.getLogger(FacebookDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            if(em!=null|em.isOpen())
+                em.close();
         }
-    }   
-        
+    }
+
     /**
     * Retorna la cantidad total de likes que contiene un post
     *
     * @param post con información del post que se encuentra recorriendo en ese momento
     * @return retorna la cantidad total de likes para un post
-    */
+    */ 
     public int getPostLikes(Post post){
         Reading reading = new Reading();
         reading.limit(Configuration.POSTS_LIMIT);
 
         try {            
-            ResponseList<Like> likes = fb.getPostLikes(post.getId(),reading);
+            ResponseList<Like> likes = facebook.getPostLikes(post.getId(),reading);
             return likes.size();
         } catch (FacebookException ex) {
             Logger.getLogger(FacebookDao.class.getName()).log(Level.SEVERE, null, ex);
@@ -120,7 +125,7 @@ public class FacebookDao {
         reading.limit(Configuration.LIKES_LIMIT);
 
         try {
-            ResponseList<Like> likes = fb.getCommentLikes(comment.getId(), reading);
+            ResponseList<Like> likes = facebook.getCommentLikes(comment.getId());
             return likes.size();
         } catch (FacebookException ex) {
             Logger.getLogger(FacebookDao.class.getName()).log(Level.SEVERE, null, ex);
