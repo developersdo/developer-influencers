20c402651f8e2130f87879ad79aa03225c79b044
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-05-04 02:24:48 -0400

    creando entidad mentions para manejar los mentions retornados por Facebook

---
+++ src/main/java/org/devdom/tracker/model/dto/Mentions.java
@@ -0,0 +1,92 @@
+package org.devdom.tracker.model.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Carlos Vásquez Polanco
+ */
+
+@Entity
+@XmlRootElement
+public class Mentions  implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "from_id")
+    private String fromId;
+    
+    @Column(name = "to_id")
+    private String toId;
+    
+    @Column(name = "created_time")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date createdTime;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the fromId
+     */
+    public String getFromId() {
+        return fromId;
+    }
+
+    /**
+     * @param fromId the fromId to set
+     */
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    /**
+     * @return the toId
+     */
+    public String getToId() {
+        return toId;
+    }
+
+    /**
+     * @param toId the toId to set
+     */
+    public void setToId(String toId) {
+        this.toId = toId;
+    }
+
+    /**
+     * @return the createdTime
+     */
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    /**
+     * @param createdTime the createdTime to set
+     */
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+}
