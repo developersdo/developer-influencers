68107830178fd0800c63a9940cb38e43b3719fd5
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-05-10 17:19:22 -0400

    Eliminando campos innesacios de la entidad

--- src/main/java/org/devdom/tracker/model/dto/FacebookMember.java
+++ src/main/java/org/devdom/tracker/model/dto/FacebookMember.java
@@ -25,23 +25,11 @@ public class FacebookMember implements Serializable {
     @Column(name = "uid")
     private String uid = "";
     
-    @Column(name = "user_id")
-    private Long userId;
-    
     @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
     private String lastName;
-    
-    @Column(name = "quiz_authorized")
-    private boolean quizAuthorized;
-    
-    @Column(name = "authorization_code")
-    private String authorizationCode;
-    
-    @Column(name = "roles")
-    private String roles;
     
     @Column(name = "pic")
     private String pic;
@@ -117,62 +105,6 @@ public class FacebookMember implements Serializable {
      */
     public void setUid(String uid) {
         this.uid = uid;
-    }
-
-    /**
-     * @return the userId
-     */
-    public Long getUserId() {
-        return userId;
-    }
-
-    /**
-     * @param userId the userId to set
-     */
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    /**
-     * @return the quizAuthorized
-     */
-    public boolean isQuizAuthorized() {
-        return quizAuthorized;
-    }
-
-    /**
-     * @param quizAuthorized the quizAuthorized to set
-     */
-    public void setQuizAuthorized(boolean quizAuthorized) {
-        this.quizAuthorized = quizAuthorized;
-    }
-
-    /**
-     * @return the authorizationCode
-     */
-    public String getAuthorizationCode() {
-        return authorizationCode;
-    }
-
-    /**
-     * @param authorizationCode the authorizationCode to set
-     */
-    public void setAuthorizationCode(String authorizationCode) {
-        this.authorizationCode = authorizationCode;
-    }
-
-    /**
-     * @return the roles
-     */
-    public String getRoles() {
-        return roles;
-    }
-
-    /**
-     * @param roles the roles to set
-     */
-    public void setRoles(String roles) {
-        this.roles = roles;
     }
 
     /**
