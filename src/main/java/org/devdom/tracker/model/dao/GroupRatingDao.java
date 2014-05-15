7671fca6467cbd52ec3a83630b9e317d527b5ffa
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-05-15 07:54:34 -0400

    Agregando DAO y DTO para manejo de información de grupo

---
+++ src/main/java/org/devdom/tracker/model/dao/GroupRatingDao.java
@@ -0,0 +1,48 @@
+package org.devdom.tracker.model.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.devdom.tracker.model.dto.GroupRating;
+import org.devdom.tracker.util.Configuration;
+
+/**
+ *
+ * @author Carlos Vásquez Polanco
+ */
+public class GroupRatingDao {
+    
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    
+    public GroupRatingDao(){
+        
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public EntityManager getEntityManager(){
+        return emf.createEntityManager(Configuration.JPAConfig());
+    }
+    
+    /**
+     * Listado de grupos con el rating que tiene el developer en cada uno de ellos
+     * 
+     * @param fromId
+     * @return
+     * @throws Exception 
+     */
+    public List<GroupRating> findGroupsRatingByUserId(String fromId)throws Exception{
+        EntityManager em = getEntityManager();
+        try{
+            return (List<GroupRating>) em.createNamedQuery("GroupRating.findGroupsRatingByUserId")
+                    .setParameter("from_id", fromId)
+                    .getResultList();
+        }finally{
+            if(em!=null|em.isOpen())
+                em.close();
+        }
+    }
+}
