85fe347285d9f463f3c24aed282e36f71cf86f48
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-05-15 23:09:09 -0400

    Renombrando clase

--- src/main/java/org/devdom/tracker/model/dao/InfluencerDao.java
+++ src/main/java/org/devdom/tracker/model/dao/InfluencerDao.java
@@ -4,18 +4,18 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import org.devdom.tracker.model.dto.Influencers;
+import org.devdom.tracker.model.dto.Influencer;
 import org.devdom.tracker.util.Configuration;
 
 /**
  *
  * @author Carlos Vásquez Polanco
  */
-public class InfluencersDao {
+public class InfluencerDao {
     
     private static final long serialVersionUID = 1L;
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
-    public InfluencersDao(){ }
+    public InfluencerDao(){ }
     
     /**
      * 
@@ -31,7 +31,7 @@ public class InfluencersDao {
      * @return
      * @throws Exception 
      */
-    public List<Influencers> findTop20DevsInfluents() throws Exception{
+    public List<Influencer> findTop20DevsInfluents() throws Exception{
         EntityManager em = getEntityManager();
         try{
             return em.createNamedQuery("Influencers.findTop10DevsInfluents").getResultList();
@@ -50,10 +50,10 @@ public class InfluencersDao {
      * @return
      * @throws Exception 
      */
-    public List<Influencers> findPositionCarruselByUserIdAndGroupId(String fromId, String groupId) throws Exception{
+    public List<Influencer> findPositionCarruselByUserIdAndGroupId(String fromId, String groupId) throws Exception{
         EntityManager em = getEntityManager();
         try{
-            return (List<Influencers>)em.createNamedQuery("Influencers.findPositionCarruselByUserIdAndGroupId")
+            return (List<Influencer>)em.createNamedQuery("Influencers.findPositionCarruselByUserIdAndGroupId")
                     .setParameter("from_id", fromId)
                     .setParameter("group_id", groupId)
                     .getResultList();
