e948a6dfb5b5c2ad1aed1fb59aeb569973eb4478
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-05-16 08:41:55 -0400

    Nuevo bean para manejar el top 20 de resultados

---
+++ src/main/java/org/devdom/tracker/bean/TopController.java
@@ -0,0 +1,38 @@
+package org.devdom.tracker.bean;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import org.devdom.tracker.model.dao.InfluencerDao;
+import org.devdom.tracker.model.dto.Influencer;
+
+/**
+ *
+ * @author Carlos Vásquez Polanco
+ */ 
+@ManagedBean
+public class TopController {
+
+    /**
+     * Listado de los 20 developers más influyentes de todos los grupos
+     * @return 
+     */
+    public List<Influencer> getTop20(){
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        Map<String,String> request = externalContext.getRequestParameterMap();
+        
+        String groupId = request.get("g");
+        try {
+            InfluencerDao daoInf = new InfluencerDao();
+            return daoInf.findTop20DevsInfluents(groupId);
+        } catch (Exception ex) {
+            Logger.getLogger(InfluencerController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
