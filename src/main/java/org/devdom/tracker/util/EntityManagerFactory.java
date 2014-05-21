71b872dbdb1b2f58e6ea050404ad165a2589084d
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-05-21 01:30:02 -0400

    Redefiniendo la ubicación de donde se extraera el nombre de la unidad de persistencia que se consumirá

--- src/main/java/org/devdom/tracker/util/EntityManagerFactory.java
+++ src/main/java/org/devdom/tracker/util/EntityManagerFactory.java
@@ -8,12 +8,11 @@ import javax.persistence.Persistence;
  * @author Carlos Vasquez Polanco
  */
 public class EntityManagerFactory{
-    
-    private final String PERSISTENCE_UNIT = "jpa";
+
     private final javax.persistence.EntityManagerFactory emf;
 
     public EntityManagerFactory(){
-        emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, Configuration.JPAConfig());
+        emf = Persistence.createEntityManagerFactory(Configuration.JPA_PU, Configuration.JPAConfig());
     }
 
     public EntityManager getEntityManager(){
